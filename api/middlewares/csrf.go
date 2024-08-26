bd82fe22fb2414ea4af4246073371461969f1e71
bintangmesir <bintangmesir@gmail.com>
2024-08-26 20:41:03 +0700

    refactor:fix domain token csrf

--- api/middlewares/csrf.go
+++ api/middlewares/csrf.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"server/initialize"
+	"strconv"
 	"time"
 
 	"github.com/gofiber/fiber/v2/middleware/csrf"
@@ -8,11 +10,14 @@ import (
 )
 
 func CSRFConfigs() csrf.Config{
+    protocol, _ :=strconv.ParseBool(initialize.ENV_POTOCOL_HTTPS)
+    
     csrfConfig := csrf.Config{
         KeyLookup:      "header:X-Csrf-Token",
         CookieName:     "csrf_token",
-	    CookieSameSite: "Lax",
         Expiration:     1 * time.Hour,        
+        CookieSameSite: "None", 
+        CookieSecure:   protocol,
         CookieDomain:   "localhost",
         KeyGenerator:   utils.UUIDv4,
     }
